ff358f18ce91d00f16f13062c7827d5be27eda09
Dennisbonke <dennisb@kabelfoon.net>
2014-09-11 18:35:00 +0200

    episode 5

--- src/main/java/com/Dennisbonke/LetsModReboot/LetsModReboot.java
+++ src/main/java/com/Dennisbonke/LetsModReboot/LetsModReboot.java
@@ -1,19 +1,20 @@
 package com.Dennisbonke.LetsModReboot;
 
 import com.Dennisbonke.LetsModReboot.proxy.IProxy;
+import com.Dennisbonke.LetsModReboot.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid ="LetsmodReboot", name = "Lets Mod Reboot", version = "1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class LetsModReboot
 {
-    @Mod.Instance("LetsModreboot")
+    @Mod.Instance(Reference.MOD_ID)
     public static LetsModReboot instance;
 
-    @SidedProxy(clientSide = "com.LetsmodReboot.proxy.ClientProxy", serverSide = "com.LetsModReboot.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
---
+++ src/main/java/com/Dennisbonke/LetsModReboot/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.Dennisbonke.LetsModReboot.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "LetsModReboot";
+    public static final String MOD_NAME = "Lets Mod Reboot";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.LetsModReboot.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.LetsModReboot.proxy.ServerProxy";
+}
